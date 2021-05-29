4f388c00df909be33ee11dc5e8fb25b2c5f6ddd0
Dennis Johnson <densarjohn@gmail.com>
2021-05-29 22:24:55 +0530

    Basic window and static triangle examples

---
+++ triangle/main.cpp
@@ -0,0 +1,79 @@
+//
+//  main.cpp
+//  Triangle
+//
+//  Created by Dennis on 26/05/21.
+//  Draw a triangle
+
+#include <iostream>
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+const GLint WIDTH = 600, HEIGHT = 480;
+
+void processKeypress(GLFWwindow *window){
+    // Close window if ESC key is pressed
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+int main(int argc, const char * argv[]) {
+    
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    
+    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Learning OpenGL", nullptr, nullptr);
+    
+    // Adjustment for Retina Display MacBooks/Macs
+    int screenWidth, screenHeight;
+    glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+    
+    if(window == nullptr){
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        
+        return -1;
+    }
+    
+    glfwMakeContextCurrent(window);
+    glewExperimental = GL_TRUE;
+    
+    if(glewInit() !=  GLEW_OK){
+        std::cout << "Failed to initialise GLEW" << std::endl;
+        return -1;
+    }
+    
+    glViewport(0, 0, screenWidth, screenHeight);
+    
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+    
+    // Render loop
+    while(!glfwWindowShouldClose(window)){
+        
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        glBegin(GL_TRIANGLES);
+        glVertex2f( -0.7, -0.5 );
+        glVertex2f( 0.7, -0.5 );
+        glVertex2f( 0, 0.7 );
+        glEnd();
+        
+        processKeypress(window);
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+    
+    glfwTerminate();
+    
+    return 0;
+}
