77189489cb2fc5e3ddd94501f1a3755d815a5838
Dennis Johnson <densarjohn@gmail.com>
2021-06-06 17:33:53 +0530

    Added vertex shader to triangle example

--- triangle/main.cpp
+++ triangle/main.cpp
@@ -12,6 +12,13 @@
 
 const GLint WIDTH = 600, HEIGHT = 480;
 
+const char* vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
 void processKeypress(GLFWwindow *window){
     // Close window if ESC key is pressed
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
@@ -55,6 +62,33 @@ int main(int argc, const char * argv[]) {
          0.5f, -0.5f, 0.0f,
          0.0f,  0.5f, 0.0f
     };
+    
+    // Vertex buffer object to handle memory
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    
+    // Copy vertex data into buffer memory
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    // Create a shader object
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    
+    // Attach shader source code and compile the shader
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    
+    // Stuff to check whether compilation was successful or not
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    
+    if(!success){
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::Compilation Failed\n" << infoLog << std::endl;
+    }
+    
     
     // Render loop
     while(!glfwWindowShouldClose(window)){
